9741681f769c0d915b79c6770c42b10bc7fe5960
Levi Ramos <leviramosgabriel2021@gmail.com>
2025-09-25 00:39:34 -0400

    search page + search endpoint + prisma setup

---
+++ webapp/src/app/api/grants/route.ts
@@ -0,0 +1,20 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const supabase = await createClient();
+    const { data, error } = await supabase.from("opportunities").select("*");
+
+    if (error) {
+      return NextResponse.json({ error }, { status: 500 });
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
