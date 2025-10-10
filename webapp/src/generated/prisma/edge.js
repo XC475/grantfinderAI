5611044caf4a772a3299651bdb35b9d7ed6a3973
Levi Ramos <leviramosgabriel2021@gmail.com>
2025-10-10 18:22:47 -0400

    regenerated prisma

--- webapp/src/generated/prisma/edge.js
+++ webapp/src/generated/prisma/edge.js
@@ -343,7 +343,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -353,6 +353,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- webapp/src/generated/prisma/index.js
+++ webapp/src/generated/prisma/index.js
@@ -344,7 +344,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -354,6 +354,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
